cbea46d85b9d498b34a72d045d1f6d878c95c580
josesaavedra2027 <138131400+josesaavedra2027@users.noreply.github.com>
2023-06-29 08:36:10 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+var fristname = "jose"
+var lastname = "Polo"
+var numberofsibling = 4
+var haspets = true;
+
+
+console. log (fristname)
+console. log (lastname)
+console. log (numberofsibling)
+console. log (haspets) 
+
+if(haspets === true){
+    console.log("they all have four legs"); 
+}
+
+wild (1 )
+
+var button = document . getElementById("y")
+var body = document . body
+var h1 = document. getElementById("there no friend for u ")
+body .setAttribute("style")
+
+ hi .innerHTML = "pg.jpg"
+
+body. setAttribute("style", "background-color:black");
+})
